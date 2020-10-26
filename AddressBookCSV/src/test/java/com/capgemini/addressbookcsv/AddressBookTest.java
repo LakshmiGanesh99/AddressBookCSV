b5e3917b5b1f8c15448634559e3855fac75c676d
LakshmiGanesh <71899781+LakshmiGanesh99@users.noreply.github.com>
2020-10-26 08:27:36 +0530

    [LakshmiGanesh] add: ability to read and write CSV

---
+++ AddressBookCSV/src/test/java/com/capgemini/addressbookcsv/AddressBookTest.java
@@ -0,0 +1,13 @@
+package com.capgemini.addressbookcsv;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AddressBookTest {
+	@Test
+	public void given3Contact_WhenWrittenTo_CSVFileShouldPass() {
+		AddressBookCSV addressBookCSVobject = new AddressBookCSV("src/main/resources/AddressBookCSV.csv");
+		int readCSVContacts = addressBookCSVobject.readAddressBookFromCSVFile();
+		Assert.assertEquals(3, readCSVContacts);
+	}
+}
